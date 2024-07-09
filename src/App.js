04b0e81f48f9906f4c72bd859a78d3c1d27245f7
Beytullah AKAY <larkvinanemo@gmail.com>
2024-07-09 09:26:05 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Homepage from './page/homepage';
 import Login from './page/Login';
 import Register from './page/Register';
 import ToArticle from './page/listenarticle'
+import React from 'react';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Homepage/>} />
-      <Route path='/Login' element={<Login/>} />
-      <Route path='/Register' element={<Register/>} />
-      <Route path='/Makaledinle' element={<ToArticle/>} />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Register' element={<Register />} />
+        <Route path='/Makaledinle' element={<ToArticle />} />
 
 
 
-    </Routes>
+      </Routes>
     </BrowserRouter>
   );
 }
