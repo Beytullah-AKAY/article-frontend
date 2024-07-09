6878fd284a3eaaa5d7d1c972d45b07399ddc4e38
Beytullah AKAY <larkvinanemo@gmail.com>
2024-07-09 12:48:43 +0300

    son

--- src/api/getArticlefROMuRL.js
+++ src/api/getArticlefROMuRL.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getArticleFromURL = async (articleId, Identity) => {
     console.log(articleId, Identity);
     return await axios.get(
-        `${process.env.REACT_APP_API_URL}/makaledinle?article_id=${articleId}&Identity=${Identity}`,
+        `${process.env.REACT_APP_API_URL}/makaledinle?articleId=${articleId}&Identity=${Identity}`,
         // {
         //     data: {
         //         ipAddress: ipAddress
